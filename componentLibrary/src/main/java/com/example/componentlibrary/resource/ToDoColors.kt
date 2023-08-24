b14f431fed883ed1ddaf85fdf14abbb40b99036c
Samuel Ochuba <68715929+kolanse@users.noreply.github.com>
2023-08-24 19:18:14 +0100

    added base colors

---
+++ componentLibrary/src/main/java/com/example/componentlibrary/resource/ToDoColors.kt
@@ -0,0 +1,67 @@
+package com.example.componentlibrary.resource
+
+import androidx.compose.ui.graphics.Color
+
+object ToDoColors {
+
+    // Primary Colors
+    // to remove Color(0xFF)
+    val black = Color(0xFF000000)
+    val grey900 = Color(0xFF2B2B2B)
+    val grey800 = Color(0xFF4B4B4B)
+    val grey700 = Color(0xFF5B5B5B)
+    val grey600 = Color(0xFF6B6B6B)
+    val grey500 = Color(0xFF7B7B7B)
+    val grey400 = Color(0xFF8B8B8B)
+    val grey300 = Color(0xFF9B9B9B)
+    val grey200 = Color(0xFFA6A6A6)
+    val grey100 = Color(0xFFBFBFBF)
+    val grey50 = Color(0xFFDCDCDC)
+    val white = Color(0xFFffffff)
+
+    // Secondary 01 Colors
+    val blue900 = Color(0xFF099EBE)
+    val blue800 = Color(0xFF19B1D2)
+    val blue700 = Color(0xFF26C3E5)
+    val blue600 = Color(0xFF45D8F9)
+    val blue500 = Color(0xFF5AE1FF)
+    val blue400 = Color(0xFF7BE7FF)
+    val blue300 = Color(0xFF99EDFF)
+    val blue200 = Color(0xFFB6F2FF)
+    val blue100 = Color(0xFFC7F5FF)
+    val blue50 = Color(0xFFDDF8FE)
+
+    // Secondary 02 Colors
+    val yellow800 = Color(0xFFE3BF00)
+    val yellow700 = Color(0xFFF2CC00)
+    val yellow600 = Color(0xFFF9D30B)
+    val yellow500 = Color(0xFFFFDC26)
+    val yellow400 = Color(0xFFFFE141)
+    val yellow300 = Color(0xFFFE55B)
+    val yellow200 = Color(0xFFFFEB80)
+    val yellow100 = Color(0xFFFFF09F)
+    val yellow50 = Color(0xFFFAEFB4)
+
+    // Secondary 03 Colors
+
+    val red800 = Color(0xFFAC2020)
+    val red700 = Color(0xFFC62525)
+    val red600 = Color(0xFFDE2B2B)
+    val red500 = Color(0xFFF03B3B)
+    val red400 = Color(0xFFFD5353)
+    val red300 = Color(0xFFFF6B6B)
+    val red200 = Color(0xFFFF8383)
+    val red100 = Color(0xFFFF9C9C)
+    val red50 = Color(0xFFFFBBBB)
+
+    // Secondary 04 Colors
+    val green800 = Color(0xFF009E82)
+    val green700 = Color(0xFF02B796)
+    val green600 = Color(0xFF01D1AC)
+    val green500 = Color(0xFF04EAC0)
+    val green400 = Color(0xFF11F9CF)
+    val green300 = Color(0xFF31FFDA)
+    val green200 = Color(0xFF57FFE1)
+    val green100 = Color(0xFF80FFE8)
+    val green50 = Color(0xFFA6FFEF)
+}
